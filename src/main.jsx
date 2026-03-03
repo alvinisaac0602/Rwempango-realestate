a1b3a8b9beffc1b718586ad5b5ca89c490c39a1a
alvinisaac0602 <kiizaisaacalvin256@gmail.com>
2026-03-03 15:32:59 +0300

    anatytics adjusted

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,12 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
-import {Analtyics} from '@vercel/analytics/react';
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import './index.css';
+import App from './App.jsx';
+import { Analytics } from '@vercel/analytics/react';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
-    <Analtyics />
-  </StrictMode>,
-)
+    <Analytics />
+  </StrictMode>
+);
